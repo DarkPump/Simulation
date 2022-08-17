c36e8cfed617e17268fd200a7a1fecde8998d7fb
Patryk Szarota <szarota.patryk@gmail.com>
2022-08-17 20:01:00 +0200

    Added random movement to the agent

--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -7,14 +7,55 @@ public class AgentMovement : MonoBehaviour
 {
     [SerializeField] private Transform goal;
     [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float walkRadius = 1f;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    private void Start()
+    private void Update()
     {
-        agent.destination = goal.position;
+        if (!agent.hasPath)
+            agent.SetDestination(GetRandomPoint(transform, walkRadius));
     }
+
+    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, 1))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+    
+    public Vector3 GetRandomPoint(Transform point = null, float radius = 0)
+    {
+        Vector3 _point;
+
+        if (RandomPoint(point == null ? transform.position : point.position, radius == 0 ? walkRadius : radius, out _point))
+        {
+            Debug.DrawRay(_point, Vector3.up, Color.black, 1);
+
+            return _point;
+        }
+
+        if (point == null)
+            return Vector3.zero;
+        else
+            return point.position;
+    }
+
+    #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, walkRadius);
+        }
+
+    #endif
 }
