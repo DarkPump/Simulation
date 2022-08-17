90c4d587aa926ee6ebaec6c38a62935607413d41
Patryk Szarota <szarota.patryk@gmail.com>
2022-08-17 23:11:31 +0200

    Added MarkoPolo button and InputField, fixed UI to fit all screen sizes

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Button textButton;
+    [SerializeField] TMP_InputField textBox;
+
+    public void PopulateTextBox()
+    {
+        for(int i = 1; i < 101; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+                textBox.text += "MarkoPolo ";
+            else if(i % 3 == 0)
+                textBox.text += "Marko ";
+            else if(i % 5 == 0)
+                textBox.text += "Polo ";
+            else
+                textBox.text += i.ToString() + " ";
+        }
+        textButton.interactable = false;
+    }
+}
